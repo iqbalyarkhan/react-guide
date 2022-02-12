3f3573ea73b2802bf50d40db4732e24ef2b64548
iqbalyarkhan <iqbal.khan@utdallas.edu>
2022-02-11 19:30:40 -0600

    pass state to children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-function App() {
+function MyButton({ count, onClick }: { count: any; onClick: any }) {
+  return <button onClick={onClick}>Clicked count: {count}</button>;
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <h1>Hello react!</h1>
+      <h1>Welcome to React</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
-
-export default App;
